f3287af9f73af8978a46a94a29f4834d8aedb389
AHUENEN2008 <ss576285@gmail.com>
2025-02-04 22:48:26 +0100

    Feat(Graphql):0.000.02 Changes: *Apollo installed and configurated in docker

---
+++ src/core/config/graphql.config.ts
@@ -0,0 +1,16 @@
+import { ConfigService } from '@nestjs/config'
+import { ApolloDriverConfig } from '@nestjs/apollo'
+import { isDev } from '@/src/shared/utils/is-dev.util'
+import {join} from "path"
+import * as process from 'node:process'
+
+export function getGraphQLConfig(configService:ConfigService)
+:ApolloDriverConfig{
+	return {
+		playground:isDev(configService),
+		path:configService.getOrThrow<string>('GRAPHQL_PREFIX'),
+		autoSchemaFile: join(process.cwd(), 'src/core/graphql/schema.gql'),
+		sortSchema:true,
+		context: ({ req,res }) =>({req,res})
+	}
+}
